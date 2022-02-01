c72d4dedb5f1f0ddafcc5424a49fca168ccdf1be
bhaktibuana <bhaktibuana19@gmail.com>
2022-02-01 20:38:39 +0700

    create API for get products thumbnail

--- src/index.js
+++ src/index.js
@@ -15,10 +15,61 @@ app.get("/", (req, res) => {
   });
 });
 
-// test db connection
-app.get("/get", (req, res) => {
-  const dbQuery = "SELECT * FROM product";
+// get count of all products
+app.get("/get/product-count", (req, res) => {
+  const dbQuery = `SELECT COUNT(*) AS total FROM product_code AS pc JOIN product AS p ON pc.product_id = p.id JOIN discount AS d ON pc.code = d.product_code_code`;
   db.query(dbQuery, (err, result) => {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+// get 10 product list for product thumbnail card
+app.get("/get/product-thumbnail/:index", (req, res) => {
+  const index = parseInt(req.params.index);
+
+  const dbQuery = `SELECT pc.code, p.product_name, p.price, d.discount_value FROM product_code AS pc JOIN product AS p ON pc.product_id = p.id JOIN discount AS d ON pc.code = d.product_code_code ORDER BY p.id ASC LIMIT ?, 10;`;
+  db.query(dbQuery, [index], (err, result) => {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+// get count of products with category
+app.get("/get/product-count-category", (req, res) => {
+  const paramsArr = Object.keys(req.query).map((key) => req.query[key]);
+  const paramsArrLen = paramsArr.length;
+
+  const dbQuery =
+    paramsArrLen > 1
+      ? `SELECT COUNT(*) AS total FROM product_code AS pc JOIN product AS p ON pc.product_id = p.id JOIN discount AS d ON pc.code = d.product_code_code WHERE p.id IN (SELECT product_id FROM (SELECT * FROM product_tag WHERE tag_id IN ((SELECT id FROM tag WHERE tag_name IN (?)))) AS a GROUP BY product_id HAVING COUNT(*) > 1);`
+      : `SELECT COUNT(*) AS total FROM product_code AS pc JOIN product AS p ON pc.product_id = p.id JOIN discount AS d ON pc.code = d.product_code_code WHERE p.id IN (SELECT product_id FROM (SELECT * FROM product_tag WHERE tag_id IN ((SELECT id FROM tag WHERE tag_name IN (?)))) AS a GROUP BY product_id HAVING COUNT(*) > 0);`;
+  db.query(dbQuery, [paramsArr], (err, result) => {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+// get 10 product list for product thumbnail with category
+app.get("/get/product-thumbnail-category/:index", (req, res) => {
+  const index = parseInt(req.params.index);
+  const paramsArr = Object.keys(req.query).map((key) => req.query[key]);
+  const paramsArrLen = paramsArr.length;
+
+  const dbQuery =
+    paramsArrLen > 1
+      ? `SELECT pc.code, p.product_name, p.price, d.discount_value FROM product_code AS pc JOIN product AS p ON pc.product_id = p.id JOIN discount AS d ON pc.code = d.product_code_code WHERE p.id IN (SELECT product_id FROM (SELECT * FROM product_tag WHERE tag_id IN ((SELECT id FROM tag WHERE tag_name IN (?)))) AS a GROUP BY product_id HAVING COUNT(*) > 1) ORDER BY p.id ASC LIMIT ?, 10;`
+      : `SELECT pc.code, p.product_name, p.price, d.discount_value FROM product_code AS pc JOIN product AS p ON pc.product_id = p.id JOIN discount AS d ON pc.code = d.product_code_code WHERE p.id IN (SELECT product_id FROM (SELECT * FROM product_tag WHERE tag_id IN ((SELECT id FROM tag WHERE tag_name IN (?)))) AS a GROUP BY product_id HAVING COUNT(*) > 0) ORDER BY p.id ASC LIMIT ?, 10;`;
+  db.query(dbQuery, [paramsArr, index], (err, result) => {
     if (err) {
       res.status(500).json(err);
     } else {
